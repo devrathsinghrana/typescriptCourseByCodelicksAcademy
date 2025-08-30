5d31f71ac16caa93f263685d75ebe701e3715262
BugBuster <devrath.rana@bold.com>
2025-08-30 23:51:08 +0530

    Use genrics for function type

---
+++ src/12_getByIndexFunction.ts
@@ -0,0 +1,8 @@
+function getByIndex<T, U>(index: number, value1: T, value2: U): T | U {
+  return index === 0 ? value1 : value2;
+}
+
+const value1 = getByIndex(0, true, false);
+const value2 = getByIndex(1, 1, 2);
+const value3 = getByIndex(0, 2, "example string");
+const value4 = getByIndex(1, true, 7);
